e5ceb02ec28fa3b4230d7b0258c80b15ab8dec54
1363Rasoul <rasol.hoseyni@yahoo.com>
2026-06-11 17:33:11 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 from liquidity import detect_sweep
 from mss import detect_mss
+from score import calculate_score
 
-print("LIT Crypto Bot Started")
+highs = [100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,125]
+lows = [90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110]
+
+closes = [100,101,102,103,104,105]
+
+sweep = detect_sweep(highs, lows)
+mss = detect_mss(closes)
+
+score = calculate_score(
+    sweep=sweep,
+    mss=mss
+)
+
+print("Sweep:", sweep)
+print("MSS:", mss)
+print("Score:", score)
